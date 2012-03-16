8ed281778f0ad355f0c04003e234ca30951998a5
Doru Catalin Budai <doru@lessonplanet.com>
2012-03-16 14:13:41 +0200

    Added a User factory and test for show page

--- Gemfile
+++ Gemfile
@@ -31,4 +31,5 @@ end
 group :test do
   gem 'rspec', '~> 2.8'
   gem 'webrat'
+  gem 'factory_girl_rails'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Cornel Les"
+  user.email                 "cornel.les@softvision.ro"
+  user.password              "secret"
+  user.password_confirmation "secret"
+end
