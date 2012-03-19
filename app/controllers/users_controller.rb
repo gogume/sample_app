75892dc5283f35dcaf0a1ab1271f9c9516d1ead2
Doru Catalin Budai <doru@lessonplanet.com>
2012-03-19 15:16:23 +0200

    Working index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+
+  def index
+    @users = User.all
+    @title = "All users"
+  end
 
   def show
     @user  = User.find(params[:id])
@@ -44,8 +49,8 @@ class UsersController < ApplicationController
       deny_access unless signed_in?
     end
 
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
-  end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
