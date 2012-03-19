f4fe1f1c81f931541478da9f16271c97a94bbae2
Doru Catalin Budai <doru@lessonplanet.com>
2012-03-19 12:00:05 +0200

    Completed signin/out

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe "Users" do
-
   describe "signup" do
-
     describe "failure" do
-
       it "should not make a new user" do
         lambda do
           visit signup_path
@@ -34,7 +31,31 @@ describe "Users" do
         end.should change(User, :count).by(1)
       end
     end
-
   end
 
+  describe "signin" do
+    describe "failure" do
+      it "should not sign the user in" do
+        visit signin_path
+        fill_in :email,    :with => ""
+        fill_in :password, :with => ""
+        click_button
+        response.should have_selector('div.flash.error', :content => "Invalid")
+        response.should render_template('sessions/new')
+      end
+    end
+
+    describe "success" do
+      it "should sign the user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in :email,    :with => user.email
+        fill_in :password, :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
+  end
 end
