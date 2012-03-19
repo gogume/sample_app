cbe1aa515bb1fbdb0a9ef3363ae526876bd63c7d
Doru Catalin Budai <doru@lessonplanet.com>
2012-03-19 18:29:47 +0200

    Added a Micropost model with User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
 
+  has_many :microposts, :dependent => :destroy
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name,  :presence => true,
@@ -12,6 +14,7 @@ class User < ActiveRecord::Base
   validates :password, :presence => true,
                        :confirmation => true,
                        :length   => { :within => 6..40 }
+
   before_save :encrypt_password
 
   def has_password?(submitted_password)
